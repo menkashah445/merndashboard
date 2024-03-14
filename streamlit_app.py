e27a1b78cbf0427ec5767e0cb27bff23bf05da11
Menka shah <105760147+menkashah445@users.noreply.github.com>
2024-03-14 12:47:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,28 @@
+import streamlit as st
+
+# Create an empty container
+placeholder = st.empty()
+
+actual_email = "pawar@gmail.com"
+actual_password = "0987654321"
+
+with placeholder.form("login"):
+    # st.markdown("#### Enter your credentials")
+    st.markdown(actual_email, actual_password)
+    email = st.text_input("Email")
+    password = st.text_input("Password", type="password")
+    submit = st.form_submit_button("Login")
+  if submit and email == actual_email and password == actual_password:
+    # If the form is submitted and the email and password are correct,
+    # clear the form/container and display a success message
+    placeholder.empty()
+    st.success("Login successful")
+elif submit and email != actual_email and password != actual_password:
+    st.error("Login failed")
+else:
+    pass
+
+
+
+
 
