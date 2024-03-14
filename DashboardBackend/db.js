e7a85a8d77ddf9f79f9d029b78c2f83ae11a78b8
menkashah445 <menkashah060@gmail.com>
2024-03-14 12:48:20 +0530

    change in streamfile

--- DashboardBackend/db.js
+++ DashboardBackend/db.js
@@ -1,13 +1,16 @@
 const mongoose = require("mongoose");
+
 require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+    const uri = 'mongodb://localhost:27017/dashboardapp';
+    const conn = await mongoose.connect(uri, {
       // useNewUrlParser: true,
       useUnifiedTopology: true,
       // useCreateIndex: true,
     });
+    console.log("MONGODB_URI:", process.env.MONGODB_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     console.error(`Error: ${err.message}`);
