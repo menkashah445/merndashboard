2f1b97212818fa465e7c0fd28aa8c60b395b079e
Menka shah <105760147+menkashah445@users.noreply.github.com>
2024-03-14 15:40:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
 import streamlit as st
-import pymongo
 
+import pymongo
+from pymongo.server_api import ServerApi
+
+# Connect to the DB.
 @st.experimental_singleton
 def connect_db():
-    client = pymongo.MongoClient(st.secrets["mongo"]["connection_url"])
+    client = pymongo.MongoClient(
+      st.secrets["mongo"]["connection_url"], 
+      server_api=ServerApi('1'))
     db = client.get_database('main')
-    return db
+    return db.users
 
 user_db = connect_db()
 
+# Initialize Session States.
 if 'username' not in st.session_state:
-    st.session_state.username = 'menkashah'
+       st.session_state.username = ''
 if 'form' not in st.session_state:
-    st.session_state.form = ''
+       st.session_state.form = ''
 
 application = st.selectbox('Key features', ('menkashah', 'riashah'))
