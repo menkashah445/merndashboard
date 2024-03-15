f2dfd08a7c4411b0cd5f5916f67c4aaf0c96e6a6
Menka shah <105760147+menkashah445@users.noreply.github.com>
2024-03-15 07:28:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import requests
 def signup(): 
   st.title("Sign Up")
 
-    username = st.text_input("Username") 
+username = st.text_input("Username") 
 email = st.text_input("Email") 
 password = st.text_input("Password", type='password') 
 password_confirm = st.text_input("Confirm Password", type='password')
