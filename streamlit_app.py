26c38b4ad897cab459a4f326ba0b533a68dc5d4d
Menka shah <105760147+menkashah445@users.noreply.github.com>
2024-03-15 07:56:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,8 @@ def signup():
     email = st.text_input("Email")
     password = st.text_input("Password", type='password')
     password_confirm = st.text_input("Confirm Password", type='password')
+    submit = st.form_submit_button("Signup")
+
     # Add your signup logic here
 
 # Add the rest of your Streamlit app code here
